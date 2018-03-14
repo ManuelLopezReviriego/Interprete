6e53f82c1c3f409d70b59032edd4b039499596f9
ManuelLopezReviriego <34214466+ManuelLopezReviriego@users.noreply.github.com>
2018-03-14 20:15:55 +0100

    Add files via upload

---
+++ scripts/Interpreter.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Interpreter
+{
+    private Lexer lexer;
+
+    public Interpreter(string line)
+    {
+        lexer = new Lexer(line);
+    }
+
+    public int Expression()
+    {   
+        int result = Term();
+
+        while(lexer.CurrentToken.Type == TokenType.PLUS || lexer.CurrentToken.Type == TokenType.MINUS)
+        {
+            if(lexer.CurrentToken.Type == TokenType.PLUS)
+            {
+                lexer.EatToken(TokenType.PLUS);
+                result += Term();
+            }
+            else
+            {
+                lexer.EatToken(TokenType.MINUS);
+                result -= Term();
+            }
+        }
+        return result;
+    }
+
+    private int Term()
+    {
+        int result = Factor();
+
+        while (lexer.CurrentToken.Type == TokenType.MUL || lexer.CurrentToken.Type == TokenType.DIV)
+        {
+            if(lexer.CurrentToken.Type == TokenType.MUL)
+            {
+                lexer.EatToken(TokenType.MUL);
+                result *= Factor();
+            } else if(lexer.CurrentToken.Type == TokenType.DIV)
+            {
+                lexer.EatToken(TokenType.DIV);
+                result /= Factor();
+            }
+        }
+
+        return result;
+    }
+
+    private int Factor()
+    {
+        Token temp = lexer.CurrentToken;
+        if (temp.Type == TokenType.INTEGER)
+        {
+            lexer.EatToken(TokenType.INTEGER);
+            return Convert.ToInt32(temp.Value);
+        }
+        else if (temp.Type == TokenType.LEFT_PAR)
+        {
+            int res;
+            lexer.EatToken(TokenType.LEFT_PAR);
+            res = Expression();
+            lexer.EatToken(TokenType.RIGHT_PAR);
+            return res;
+        }
+        throw new Exception("Error: Unknown symbol '" + lexer.CurrentToken.Type + "'");
+    }
+
+}
---
+++ scripts/Lexer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Lexer
+{
+    private string line;
+    private int index;
+    private Token currentToken;
+
+    public Token CurrentToken => currentToken;
+
+    public Lexer(string line)
+    {
+        this.line = line;
+        index = 0;
+        currentToken = NextToken();
+    }
+
+    private void SkipWhiteSpaces()
+    {
+        while (line[index] == ' ')
+        {
+            index++;
+        }
+    }
+
+    private int GetInteger()
+    {
+        string num = "";
+        while (index < line.Length && Char.IsDigit(line[index]))
+        {
+            num += line[index];
+            index++;
+        }
+        return Convert.ToInt32(num);
+    }
+
+    public Token NextToken()
+    {
+        while (index < line.Length)
+        {
+            char c = line[index];
+
+            if (c == ' ')
+            {
+                SkipWhiteSpaces();
+                continue;
+            }
+
+            if (Char.IsDigit(c))
+            {
+                return new Token(TokenType.INTEGER, GetInteger().ToString()); //
+            }
+
+            switch (c)
+            {
+                case '+':
+                    index++;
+                    return Token.PLUS;
+                case '-':
+                    index++;
+                    return Token.MINUS;
+                case '*':
+                    index++;
+                    return Token.MUL;
+                case '/':
+                    index++;
+                    return Token.DIV;
+                case '(':
+                    index++;
+                    return Token.LEFT_PAR;
+                case ')':
+                    index++;
+                    return Token.RIGHT_PAR;
+            }
+
+            throw new Exception("Error: Unknown symbol '" + c + "'");
+        }
+        return Token.EOF;
+    }
+
+    public void EatToken(TokenType type)
+    {
+        if (currentToken.Type == type)
+        {
+            currentToken = NextToken();
+        }
+        else
+        {
+            throw new Exception(String.Format("Error: Unexpected token of type '{0}'.", currentToken.Type));
+        }
+    }
+}
