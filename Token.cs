ea0297f03dfe743cdeeba2740624a1a8a0176fd9
ManuelLopezReviriego <34214466+ManuelLopezReviriego@users.noreply.github.com>
2018-03-14 17:57:25 +0100

    Add files via upload

---
+++ Token.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Token
+{
+    private TokenType type;
+    private string value;
+
+    public Token(TokenType type, string value)
+    {
+        this.type = type;
+        this.value = value;
+    }
+
+    public override string ToString()
+    {
+        return String.Format("Token({0}, {1})", type, value);
+    }
+}
+
+enum TokenType { INTEGER, PLUS, MINUS, EOF }
